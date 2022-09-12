dbaa4fbdd639b3b36d94e1df8aa3c819a2e53234
ervalsa <ervalsa-san@gmail.com>
2022-09-12 14:59:45 +0800

    First Commit

---
+++ project-1/js/script.js
@@ -0,0 +1,9 @@
+$(function() {
+    $(document).on('scroll', function() {
+        if($(window).scrollTop() > 10) {
+            $('header').addClass('active');
+        } else {
+            $('header').removeClass('active');
+        }
+    });
+});
